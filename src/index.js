30606eb5d59c011d0e30c7368f777ddc0dbb33db
aovent <maxim.lipatov06@gmail.com>
2023-05-09 23:00:39 +0300

    added first script code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-var startBalance = 0;
-console.log(startBalance);
+const gemsField = document.querySelector(".stats_ultramoney");
+const balanceField = document.querySelector(".stats_money");
+const levelField = document.querySelector(".stats_level");
+const rebirthsField = document.querySelector(".rebirths_level");
+if (localStorage.getItem("AoventWebClicker") == null) {
+    const startBalance = 1000;
+    balanceField.innerHTML = `Money: ${startBalance}`;
+}
+function getNowBalance() {
+    let nowBalance = Number(String((gemsField.innerHTML)).split(": ")[1]);
+    return nowBalance;
+}
+console.log(getNowBalance());
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const startBalance: number = 0;
+const gemsField: HTMLElement = document.querySelector(".stats_ultramoney");
+const balanceField: HTMLElement = document.querySelector(".stats_money");
+const levelField: HTMLElement = document.querySelector(".stats_level");
+const rebirthsField: HTMLElement = document.querySelector(".rebirths_level");
 
-console.log(startBalance);
+if (localStorage.getItem("AoventWebClicker") == null) {
+    const startBalance: number = 1000;
+
+    balanceField.innerHTML = `Money: ${startBalance}`
+}
+
+function getNowBalance(): number {
+    let nowBalance: number = Number(String((gemsField.innerHTML)).split(": ")[1]);
+
+    return nowBalance;
+}
+
+console.log(getNowBalance());
+
+// "tsc" to compile
