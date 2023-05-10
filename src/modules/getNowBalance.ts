412a167e69dd110cdfca4e7b21aaad4bbd63e7e3
aovent <maxim.lipatov06@gmail.com>
2023-05-10 16:45:02 +0300

    commit past merge

--- src/modules/getNowBalance.ts
+++ src/modules/getNowBalance.ts
@@ -1,5 +1,3 @@
 export function getNowBalance(gemsField): number {
-    let nowBalance: number = Number(String((gemsField.innerHTML)).split(": ")[1]);
-
-    return nowBalance;
+    return Number(String((gemsField.innerHTML)).split(": ")[1]);
 }
